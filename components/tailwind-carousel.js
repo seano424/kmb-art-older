223f053a80769a2bdca2ba7d0804fd4e9ed91eba
Sean OReilly <soreilly424@gmail.com>
2021-04-30 16:29:57 -0500

    tailwind carousel in progress

--- components/tailwind-carousel.js
+++ components/tailwind-carousel.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { urlFor } from "../lib/sanity";
 
 export default function TailwindCarousel({ images }) {
+  const [show, setShow] = useState(3);
+
   const divStyle = {
     height: "400px",
   };
@@ -11,14 +13,61 @@ export default function TailwindCarousel({ images }) {
     objectPosition: "top",
     height: "400px",
   };
-  return (
-    <div style={divStyle} className="bg-indigo-100 mb-20 px-40">
+
+  const carouselImage = (
+    <>
       <img
-        className="w-full"
+        className={`carousel-images w-full transition-opacity duration-1000 ease-in-out ${
+          show !== 0 ? "opacity-0" : "opacity-100"
+        }`}
         style={imageStyle}
-        src={urlFor(images[0].image).url()}
+        src={urlFor(images[0]?.image).url()}
         alt="hello"
       />
+      <img
+        className={`carousel-images carousel-image-1 w-full transition-opacity duration-1000 ease-in-out ${
+          show !== 1 ? "opacity-0" : "opacity-100"
+        }`}
+        style={imageStyle}
+        src={urlFor(images[1]?.image).url()}
+        alt="hello"
+      />
+      <img
+        className={`carousel-images carousel-image-2 w-full transition-opacity duration-1000 ease-in-out ${
+          show !== 2 ? "opacity-0" : "opacity-100"
+        }`}
+        style={imageStyle}
+        src={urlFor(images[2]?.image).url()}
+        alt="hello"
+      />
+      <img
+        className={`carousel-images carousel-image-3 w-full transition-opacity duration-1000 ease-in-out ${
+          show !== 3 ? "opacity-0" : "opacity-100"
+        }`}
+        style={imageStyle}
+        src={urlFor(images[3]?.image).url()}
+        alt="hello"
+      />
+      <img
+        className={`carousel-images carousel-image-4 w-full transition-opacity duration-1000 ease-in-out ${
+          show !== 4 ? "opacity-0" : "opacity-100"
+        }`}
+        style={imageStyle}
+        src={urlFor(images[4]?.image).url()}
+        alt="hello"
+      />
+    </>
+  );
+
+  setTimeout(() => {
+    const num = show > images.length - 2 ? 0 : show + 1;
+    // const num = show < 2 ? show + 1 : 0;
+    setShow(num);
+  }, 8000);
+
+  return (
+    <div style={divStyle} className="bg-indigo-100 mb-20 px-40">
+      {carouselImage}
     </div>
   );
 }
