b77b4d0dcc0c1abe0255b055471bab833e4e9bb0
Sean OReilly <soreilly424@gmail.com>
2021-04-30 20:07:30 -0500

    added carousel

--- components/tailwind-carousel.js
+++ components/tailwind-carousel.js
@@ -1,17 +1,43 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { FaArrowLeft } from "react-icons/fa";
+import { FaArrowRight } from "react-icons/fa";
 import { urlFor } from "../lib/sanity";
 
 export default function TailwindCarousel({ images }) {
   const [show, setShow] = useState(3);
 
-  const divStyle = {
-    height: "400px",
+  useEffect(() => {
+    let timeout = setTimeout(() => {
+      let num = show > images.length - 2 ? 0 : show + 1;
+      setShow(num);
+    }, 8000);
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [show]);
+
+  useEffect(() => {
+    window.addEventListener("keydown", downHandler);
+
+    return () => {
+      window.removeEventListener("keydown", downHandler);
+    };
+  }, [show]);
+
+  const downHandler = ({ key }) => {
+    if (key === "ArrowLeft") {
+      show <= 0 ? setShow(images.length - 1) : setShow(show - 1);
+    }
+    if (key === "ArrowRight") {
+      show >= images.length - 1 ? setShow(0) : setShow(show + 1);
+    }
   };
 
-  const imageStyle = {
-    objectFit: "cover",
-    objectPosition: "top",
-    height: "400px",
+  const moveLeft = () => {
+    show <= 0 ? setShow(images.length - 1) : setShow(show - 1);
+  };
+  const moveRight = () => {
+    show >= images.length - 1 ? setShow(0) : setShow(show + 1);
   };
 
   const carouselImage = (
@@ -20,54 +46,51 @@ export default function TailwindCarousel({ images }) {
         className={`carousel-images w-full transition-opacity duration-1000 ease-in-out ${
           show !== 0 ? "opacity-0" : "opacity-100"
         }`}
-        style={imageStyle}
         src={urlFor(images[0]?.image).url()}
-        alt="hello"
+        alt={images[0]?.content}
       />
       <img
         className={`carousel-images carousel-image-1 w-full transition-opacity duration-1000 ease-in-out ${
           show !== 1 ? "opacity-0" : "opacity-100"
         }`}
-        style={imageStyle}
         src={urlFor(images[1]?.image).url()}
-        alt="hello"
+        alt={images[1]?.content}
       />
       <img
         className={`carousel-images carousel-image-2 w-full transition-opacity duration-1000 ease-in-out ${
           show !== 2 ? "opacity-0" : "opacity-100"
         }`}
-        style={imageStyle}
         src={urlFor(images[2]?.image).url()}
-        alt="hello"
+        alt={images[2]?.content}
       />
       <img
         className={`carousel-images carousel-image-3 w-full transition-opacity duration-1000 ease-in-out ${
           show !== 3 ? "opacity-0" : "opacity-100"
         }`}
-        style={imageStyle}
         src={urlFor(images[3]?.image).url()}
-        alt="hello"
+        alt={images[3]?.content}
       />
       <img
         className={`carousel-images carousel-image-4 w-full transition-opacity duration-1000 ease-in-out ${
           show !== 4 ? "opacity-0" : "opacity-100"
         }`}
-        style={imageStyle}
         src={urlFor(images[4]?.image).url()}
-        alt="hello"
+        alt={images[4]?.content}
       />
     </>
   );
 
-  setTimeout(() => {
-    const num = show > images.length - 2 ? 0 : show + 1;
-    // const num = show < 2 ? show + 1 : 0;
-    setShow(num);
-  }, 8000);
-
   return (
-    <div style={divStyle} className="bg-indigo-100 mb-20 px-40">
+    <div style={{ height: "400px" }} className="bg-indigo-100 mb-20 md:px-40">
+      <FaArrowLeft
+        onClick={moveLeft}
+        className="hidden md:block cursor-pointer  arrows left opacity-25 hover:opacity-75 transition-opacity duration-100 ease-in-out"
+      />
       {carouselImage}
+      <FaArrowRight
+        onClick={moveRight}
+        className="hidden md:block cursor-pointer arrows right opacity-25 hover:opacity-75 transition-opacity duration-100 ease-in-out"
+      />
     </div>
   );
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,4 @@
 import "../styles/index.css";
-import styles from "react-responsive-carousel/lib/styles/carousel.min.css";
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
