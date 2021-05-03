886a5b31678b32153f75322e66f3cb25faeba1f4
Sean OReilly <soreilly424@gmail.com>
2021-05-03 18:54:28 -0500

    added highlight to sidenav

--- components/sidebar-layout.js
+++ components/sidebar-layout.js
@@ -1,8 +1,11 @@
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
+import { useRouter } from "next/router";
 
 export default function SidebarLayout({ children }) {
+  const { pathname } = useRouter();
+  console.log(pathname);
   return (
     <>
       <div className="fixed pl-12 flex min-h-screen top-0 left-0 overflow-x-hidden flex-col w-56 flex-shrink-0">
@@ -17,37 +20,65 @@ export default function SidebarLayout({ children }) {
           </a>
         </Link>
         <Link href="/paintings">
-          <a className="text-base ml-1 font-light mb-1 hover:bg-design-yellow">
+          <a
+            className={`text-base ml-1 font-light mb-1 hover:bg-design-yellow ${
+              pathname === "/paintings" && "bg-design-yellow"
+            }`}
+          >
             Paintings
           </a>
         </Link>
         <Link href="/ink-works">
-          <a className="text-base ml-1 font-light mb-1 hover:bg-design-yellow">
+          <a
+            className={`text-base ml-1 font-light mb-1 hover:bg-design-yellow ${
+              pathname === "/ink-works" && "bg-design-yellow"
+            }`}
+          >
             Ink Works
           </a>
         </Link>
         <Link href="/charcoals">
-          <a className="text-base ml-1 font-light mb-1 hover:bg-design-yellow">
+          <a
+            className={`text-base ml-1 font-light mb-1 hover:bg-design-yellow ${
+              pathname === "/charcoals" && "bg-design-yellow"
+            }`}
+          >
             Charcoals
           </a>
         </Link>
         <Link href="#">
-          <a className="text-base ml-1 font-light mb-1 hover:bg-design-yellow">
+          <a
+            className={`text-base ml-1 font-light mb-1 hover:bg-design-yellow ${
+              pathname === "/exhibitions" && "bg-design-yellow"
+            }`}
+          >
             Exhibitions
           </a>
         </Link>
         <Link href="#">
-          <a className="text-base ml-1 font-light mb-1 hover:bg-design-yellow">
+          <a
+            className={`text-base ml-1 font-light mb-1 hover:bg-design-yellow ${
+              pathname === "/book" && "bg-design-yellow"
+            }`}
+          >
             Book
           </a>
         </Link>
         <Link href="#">
-          <a className="text-base ml-1 font-light mb-1 hover:bg-design-yellow">
+          <a
+            className={`text-base ml-1 font-light mb-1 hover:bg-design-yellow ${
+              pathname === "/about" && "bg-design-yellow"
+            }`}
+          >
             About
           </a>
         </Link>
         <Link href="#">
-          <a className="text-base ml-1 font-light mb-1 hover:bg-design-yellow">
+          <a
+            className={`text-base ml-1 font-light mb-1 hover:bg-design-yellow ${
+              pathname === "/contact" && "bg-design-yellow"
+            }`}
+          >
             Contact
           </a>
         </Link>
