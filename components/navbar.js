a7052955e8d95b9aabe32aa8590965203c73ba1e
Sean OReilly <soreilly424@gmail.com>
2021-05-05 18:49:44 -0500

    changed font

--- components/navbar.js
+++ components/navbar.js
@@ -8,64 +8,40 @@ const maxContent = {
 
 export default function Navbar() {
   return (
-    <section className="flex md:px-12 items-center">
-      <h1 className="w-full pr-56 pl-8 md:pl-16 md:pr-0 sm:block md:hidden xl:block text-center md:h-40">
+    <section className="flex md:px-12 items-center mx-32 my-8">
+      <h1 className="relative h-28 w-full">
         <Image
           src="/images/full-signature.png"
           alt="Karrie Marie Baxley Website Logo"
-          width={800}
-          height={200}
-          quality={100}
+          layout="fill"
+          objectFit="contain"
         />
       </h1>
 
       <div className="w-full hidden md:flex md:pr-28 xl:pr-0 md:h-20 items-center justify-end">
-        <Link href="/">
-          <a
-            style={maxContent}
-            className="lg:w-max mx-2 lg:mx-6 hover:text-primary text-xs"
-          >
-            ABOUT
+        <Link href="/paintings">
+          <a style={maxContent} className="lg:w-max mx-2 md:mx-4 ">
+            Artwork
           </a>
         </Link>
         <Link href="/paintings">
-          <a
-            style={maxContent}
-            className="lg:w-max mx-2 lg:mx-6 hover:text-primary text-xs"
-          >
-            PAINTINGS
-          </a>
-        </Link>
-        <Link href="/charcoals">
-          <a
-            style={maxContent}
-            className="lg:w-max mx-2 lg:mx-6 hover:text-primary text-xs"
-          >
-            CHARCOALS
-          </a>
-        </Link>
-        <Link href="/ink-works">
-          <a
-            style={maxContent}
-            className="lg:w-max mx-2 lg:mx-6 hover:text-primary text-xs"
-          >
-            INK WORKS
-          </a>
-        </Link>
-        <Link href="/">
-          <a
-            style={maxContent}
-            className="lg:w-max mx-2 lg:mx-6 hover:text-primary text-xs"
-          >
-            EXHIBITIONS
+          <a style={maxContent} className="lg:w-max mx-2 md:mx-4 ">
+            Blog
           </a>
         </Link>
         <Link href="/dancing-with-a-thousand-bees">
-          <a
-            style={maxContent}
-            className="lg:w-max mx-2 lg:mx-6 hover:text-primary text-xs"
-          >
-            BOOK
+          <a style={maxContent} className="lg:w-max mx-2 md:mx-4 ">
+            Book
+          </a>
+        </Link>
+        <Link href="/">
+          <a style={maxContent} className="lg:w-max mx-2 md:mx-4 ">
+            About
+          </a>
+        </Link>
+        <Link href="/">
+          <a style={maxContent} className="lg:w-max mx-2 md:mx-4 ">
+            Contact
           </a>
         </Link>
       </div>
