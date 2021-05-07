ee88779ac004615f5e83dcc1f47607c13597abd2
Sean OReilly <soreilly424@gmail.com>
2021-05-07 16:18:09 -0500

    added one less purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,6 @@ module.exports = {
   purge: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
     "./layout/**/*.{js,ts,jsx,tsx}",
     "./helpers/**/*.{js,ts,jsx,tsx}",
   ],
