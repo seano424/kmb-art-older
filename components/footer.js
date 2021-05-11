92b2152bf191e0b8330431151d48700217c05336
Sean OReilly <soreilly424@gmail.com>
2021-05-11 10:06:14 -0500

    footer spacing corrected

--- components/footer.js
+++ components/footer.js
@@ -3,45 +3,29 @@ import Image from "next/image";
 
 export default function Footer() {
   return (
-    <footer className="bg-accent-1 border-t border-accent-2 mt-auto">
-      <Container>
-        <div className="flex py-12 justify-between items-center">
-          <a href="https://www.seanoreilly.me/" target="_blank">
-            Site created by Sean O'Reilly
-          </a>
-          <p className="w-2/5">
-            Copyright © 2021-2025 Karrie Marie Arts. All Rights Reserved. All
-            images are copyrighted to their respective owners.
-          </p>
+    <footer className="bg-accent-1 border-t border-accent-2 mt-auto px-20">
+      <div className="flex py-12 justify-between items-center">
+        <a
+          className="text-base hidden lg:flex"
+          href="https://www.seanoreilly.me/"
+          target="_blank"
+        >
+          Site created by Sean O'Reilly
+        </a>
+        <p className="lg:w-2/5">
+          Copyright © 2021-2025 Karrie Marie Arts. All Rights Reserved. All
+          images are copyrighted to their respective owners.
+        </p>
+        <div className="hidden lg:flex">
           <Image
             src="/images/full-initials.png"
             alt="Karrie Marie Baxley Website Logo"
-            width={100}
-            height={50}
+            width={180}
+            height={80}
             quality={100}
           />
         </div>
-      </Container>
+      </div>
     </footer>
   );
 }
-
-// <div className="py-4 flex flex-col lg:flex-row items-center">
-//   <h3 className="text-4xl lg:text-5xl font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
-//     Site by Sean O'Reilly
-//   </h3>
-//   <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
-//     <a
-//       href="https://nextjs.org/docs/basic-features/pages"
-//       className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
-//     >
-//       Read Documentation
-//     </a>
-//     <a
-//       href={`https://github.com/vercel/next.js/tree/canary/examples/${EXAMPLE_PATH}`}
-//       className="mx-3 font-bold hover:underline"
-//     >
-//       View on GitHub
-//     </a>
-//   </div>
-// </div>
