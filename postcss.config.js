3aa5e6ae30cffc7ee8cf90ce64d123ef8634e892
Sean OReilly <soreilly424@gmail.com>
2021-05-11 11:39:22 -0500

    removed the pages from tailwind css postcss file

--- postcss.config.js
+++ postcss.config.js
@@ -6,10 +6,7 @@ module.exports = {
           [
             "@fullhuman/postcss-purgecss",
             {
-              content: [
-                "./pages/**/*.{js,jsx,ts,tsx}",
-                "./components/**/*.{js,jsx,ts,tsx}",
-              ],
+              content: ["./components/**/*.{js,jsx,ts,tsx}"],
               defaultExtractor: (content) =>
                 content.match(/[\w-/:]+(?<!:)/g) || [],
             },
