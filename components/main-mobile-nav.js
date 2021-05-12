a54f27791ad8fc05e68eb20099c1e50d0ea553f9
Sean OReilly <soreilly424@gmail.com>
2021-05-11 20:09:45 -0500

    added hover for mobile nav

--- components/main-mobile-nav.js
+++ components/main-mobile-nav.js
@@ -4,37 +4,37 @@ export default function MainMobileNav() {
   return (
     <div className="flex flex-col items-center justify-center m-12">
       <Link href="/paintings">
-        <a className="mt-4 text-4xl uppercase hover:bg-teal-300 rounded px-2">
+        <a className="mt-4 text-4xl uppercase hover:bg-green-200 rounded px-2">
           Paintings
         </a>
       </Link>
       <Link href="/charcoals">
-        <a className="mt-4 text-4xl uppercase hover:bg-teal-300 rounded px-2">
+        <a className="mt-4 text-4xl uppercase hover:bg-green-200 rounded px-2">
           Charcoals
         </a>
       </Link>
       <Link href="/ink-works">
-        <a className="mt-4 text-4xl uppercase hover:bg-teal-300 rounded px-2">
+        <a className="mt-4 text-4xl uppercase hover:bg-green-200 rounded px-2">
           Ink Works
         </a>
       </Link>
       <Link href="/dancing-with-a-thousand-bees">
-        <a className="mt-4 text-4xl uppercase hover:bg-teal-300 rounded px-2">
+        <a className="mt-4 text-4xl uppercase hover:bg-green-200 rounded px-2">
           Book
         </a>
       </Link>
       <Link href="/paintings">
-        <a className="mt-4 text-4xl uppercase hover:bg-teal-300 rounded px-2">
+        <a className="mt-4 text-4xl uppercase hover:bg-green-200 rounded px-2">
           About
         </a>
       </Link>
       <Link href="/blog">
-        <a className="mt-4 text-4xl uppercase hover:bg-teal-300 rounded px-2">
+        <a className="mt-4 text-4xl uppercase hover:bg-green-200 rounded px-2">
           Blog
         </a>
       </Link>
       <Link href="/paintings">
-        <a className="mt-4 text-4xl uppercase hover:bg-teal-300 rounded px-2">
+        <a className="mt-4 text-4xl uppercase hover:bg-green-200 rounded px-2">
           Contact
         </a>
       </Link>
