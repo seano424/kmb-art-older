187230d237117247c9555f73888f420c838ded61
Sean OReilly <soreilly424@gmail.com>
2021-05-20 08:52:37 -0500

    adding gtm module for tracking

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,15 @@
+import { useEffect } from "react";
+import TagManager from "react-gtm-module";
 import { NavProvider } from "../context/NavContext";
 import "../styles/index.css";
 import "../styles/globals.css";
 import "react-image-lightbox/style.css";
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    TagManager.initialize({ gtmId: "UA-171312622-1" });
+  }, []);
+
   return (
     <NavProvider>
       <Component {...pageProps} />
