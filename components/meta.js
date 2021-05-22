826d232ba6a9077d3515e2ee8753fb3500cb7ea1
Sean OReilly <soreilly424@gmail.com>
2021-05-22 08:15:40 -0500

    added title to meta

--- components/meta.js
+++ components/meta.js
@@ -4,6 +4,7 @@ import { CMS_NAME, HOME_OG_IMAGE_URL } from "../lib/constants";
 export default function Meta() {
   return (
     <Head>
+      <title>Karrie Marie Baxley Arts</title>
       <link rel="shortcut icon" href="/images/favicon.ico" />
       <meta name="msapplication-TileColor" content="#000000" />
       <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
