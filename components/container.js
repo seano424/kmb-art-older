7f690791824407e9f21a37a9153b38422b5f837a
Sean OReilly <soreilly424@gmail.com>
2021-09-23 15:29:48 -0500

    adding bundle

--- components/container.js
+++ components/container.js
@@ -1,7 +1,17 @@
+import { useEffect, useState } from 'react'
 import styles from './container.module.css'
 import Link from 'next/link'
 
 export default function Container({ children, background, upcomingEvent }) {
+  const [show, setShow] = useState(true)
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShow(false)
+    }, 3000)
+    return () => clearTimeout(timer)
+  }, [])
+
   return (
     <div
       style={{
@@ -10,7 +20,11 @@ export default function Container({ children, background, upcomingEvent }) {
       className={`h-screen ${styles.backgroundContainer} container mx-auto bg-gray-50 bg-opacity-20  lg:w-4/5 lg:right-0`}
     >
       {upcomingEvent && (
-        <div className="p-5 font-light text-opacity-80 tracking-wide text-gray-600  shadow-sm">
+        <div
+          className={`${
+            show ? 'opacity-100' : 'opacity-0'
+          } transition-all duration-700 hover:opacity-100 ease-out p-5 font-light text-opacity-80 hover:text-opacity-100 tracking-wide text-gray-600  shadow-sm`}
+        >
           <span className="">Upcoming Event </span>
           <span className="text-red-400 text-opacity-80">
             {upcomingEvent.title}
