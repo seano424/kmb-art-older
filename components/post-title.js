b100fcc07f772d1c49c56a7e54cc889b0470136f
Sean OReilly <soreilly424@gmail.com>
2021-09-24 12:50:10 -0500

    fixed post mobile nav title issue

--- components/post-title.js
+++ components/post-title.js
@@ -1,34 +1,23 @@
 import { useContext } from 'react'
 import { NavContext } from '../context/NavContext'
+// import Hamburger from 'hamburger-react'
+import { Squash as Hamburger } from 'hamburger-react'
 
 export default function PostTitle({ children }) {
-  const { open, toggleOpen } = useContext(NavContext)
+  const { isOpen, toggleOpen } = useContext(NavContext)
+  // const [isOpen, setOpen] = useState(false)
 
   return (
-    <div className="flex m-auto items-center justify-between uppercase font-bold text-2xl pt-10 mb-10">
+    <div className="flex items-center justify-between uppercase font-bold text-2xl pt-10 mb-10">
       <div
         onClick={toggleOpen}
-        className="lg:hidden cursor-pointer flex flex-col justify-center"
+        className="lg:hidden cursor-pointer flex gap-5 justify-between items-center w-full"
       >
-        <div
-          className={`w-8 h-1 bg-gray-900 rounded-lg transition-all duration-150 ease-linear relative ${
-            open ? `transform rotate-45` : `transform rotate-0 mt-1`
-          }`}
-        />
-        <div
-          className={`w-8 h-1 bg-gray-900 rounded-lg transition-all duration-150 ease-linear relative ${
-            open
-              ? `transform translate-x-5 opacity-0`
-              : `transform translate-x-0 opacity-100 mt-1 `
-          }`}
-        />
-        <div
-          className={`w-8 h-1 bg-gray-900 rounded-lg transition-all duration-150 ease-linear relative ${
-            open ? `transform -rotate-45 -mt-2` : `transform rotate-0 mt-1 `
-          }`}
-        />
+        <Hamburger toggled={isOpen} toggle={toggleOpen} />
       </div>
-      <div className="text-5xl text-gray-300 font-normal">{children}</div>
+      <div className="md:text-5xl text-gray-300 text-right md:text-left font-normal">
+        {children}
+      </div>
     </div>
   )
 }
