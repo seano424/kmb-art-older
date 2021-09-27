dc86f6f9335548cf552da144c8c7015c4925dd77
Sean OReilly <soreilly424@gmail.com>
2021-09-27 12:53:54 -0500

    contact page added

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -4,6 +4,7 @@ import PostTitle from '@/components/post-title'
 import Container from '@/components/container'
 import { getUpcomingEvents } from 'lib/api'
 import relevantEvents from 'utils/relevantEvents'
+import Image from 'next/image'
 
 export default function index({ upcomingEvents }) {
   const events = relevantEvents(upcomingEvents)
@@ -13,6 +14,24 @@ export default function index({ upcomingEvents }) {
       <SidebarLayout>
         <Container upcomingEvent={events ? events[0] : null} background>
           <PostTitle>Contact Karrie</PostTitle>
+          <div className="flex flex-col md:flex-row gap-5 items-center">
+            <div className="relative h-80 w-64">
+              <Image
+                src={'/images/back.jpg'}
+                layout="fill"
+                alt="contact Karrie Marie Image"
+              />
+            </div>
+            <p className="text-base">
+              Please email me at{' '}
+              <a
+                className="text-base text-red-400"
+                href="mailto:bluemoonkmo@yahoo.com"
+              >
+                bluemoonkmo@yahoo.com
+              </a>
+            </p>
+          </div>
         </Container>
       </SidebarLayout>
     </Layout>
