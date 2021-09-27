46dbf5d86b4803d1487c90de6c7f3c1ccfbbe66f
Sean OReilly <soreilly424@gmail.com>
2021-09-27 14:07:29 -0500

    changed carousel name again

--- components/Carousel.js
+++ components/Carousel.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react'
 import Link from 'next/link'
 import { urlFor } from 'lib/sanity'
 import Image from 'next/image'
-// changed name
+// changed name again
 export default function Carousel({ images }) {
   const [show, setShow] = useState(0)
 
