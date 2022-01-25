cf60db274681cec2b58756a3aea17649ab45090f
Sean OReilly <soreilly424@gmail.com>
2022-01-24 20:36:27 -0600

    added meta back into layout

--- components/layout.js
+++ components/layout.js
@@ -1,15 +1,10 @@
 import Footer from '../components/footer'
 import Meta from '../components/meta'
-import Head from 'next/head'
 
 export default function Layout({ footer, children }) {
   return (
     <>
-      <Head>
-        <title>Karrie Marie Baxley | Artist and Writer</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      {/* <Meta /> */}
+      <Meta />
       <div className="flex min-h-screen flex-col">
         <main className="flex-1">{children}</main>
         {footer && <Footer />}
