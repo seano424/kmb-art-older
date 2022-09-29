a4e89deb81c2c63081251aed01bfd77b5d26af80
Sean OReilly <soreilly424@gmail.com>
2022-09-29 08:08:50 -0500

    removing comments from schema

--- studio/schemas/schema.js
+++ studio/schemas/schema.js
@@ -18,15 +18,9 @@ import about from './about'
 import upcomingEvents from './upcomingEvents'
 import eventDate from './eventDate'
 
-// Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
-  // We name our schema
   name: 'default',
-  // Then proceed to concatenate our document type
-  // to the ones provided by any plugins that are installed
   types: schemaTypes.concat([
-    // The following are document types which will appear
-    // in the studio.
     series,
     book,
     about,
@@ -37,8 +31,6 @@ export default createSchema({
     artwork,
     upcomingEvents,
     eventDate,
-    // When added to this list, object types can be used as
-    // { type: 'typename' } in other document schemas
     blockContent,
     comment,
   ]),
