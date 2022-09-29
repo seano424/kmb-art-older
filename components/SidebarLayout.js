75b2c1b00d3eae42dac956b6d1959f8fde77f1e0
Sean OReilly <soreilly424@gmail.com>
2022-09-29 13:59:05 -0500

    adding clsx

--- components/SidebarLayout.js
+++ components/SidebarLayout.js
@@ -1,10 +1,7 @@
 import { useContext } from 'react'
-
 import Link from 'next/link'
-import Image from 'next/image'
 import { useRouter } from 'next/router'
-
-import { NavContext } from '../context/NavContext'
+import { NavContext } from '@/context/NavContext'
 import MainMobileNav from './MainMobileNav'
 
 export default function SidebarLayout({ children, background = true }) {
